dca65de2b49788facdc6609120286ba1e53740c8
diego <dgcuenca@gmail.com>
2023-05-23 15:29:05 -0500

    Fix linters errors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,8 @@
 class MessagesController < ApplicationController
-
-  def index
-  end
+  def index; end
 
   def random
-    random_message = Message.order("RANDOM()").first
+    random_message = Message.order('RANDOM()').first
     render json: { greeting: random_message&.content }
   end
 end
