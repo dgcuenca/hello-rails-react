d8c381d9e2c3aa9eba36fd9dcb596829413bc455
diego <dgcuenca@gmail.com>
2023-05-23 13:11:51 -0500

    Create endpoint that selects a random greeting

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,7 @@
+class MessagesController < ApplicationController
+
+  def random
+    random_message = Message.order("RANDOM()").first
+    render json: { greeting: random_message&.content }
+  end
+end
